09886404e6a30bb532f18d446c243e296d84787a
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-10-22 20:47:19 +0530

    Route Params

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,7 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate("Meals Overview");
+      navigation.navigate("Meals Overview", { categoryId: itemData.item.id });
     }
 
     return (
